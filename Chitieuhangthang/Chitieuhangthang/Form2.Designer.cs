46974d628f858ddef65a29f8ad77e40e80acb314
IL-vin9220 <72450290+IL-vin9220@users.noreply.github.com>
2021-08-22 11:13:45 +0700

    Add files via upload

---
+++ Chitieuhangthang/Chitieuhangthang/Form2.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Chitieuhangthang
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableDanhSach = new System.Windows.Forms.DataGridView();
+            this.stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.giamua = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ngaymua = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.thoidiem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.trocap = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tietkiem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuTable = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xóaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tong = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tableDanhSach)).BeginInit();
+            this.menuTable.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableDanhSach
+            // 
+            this.tableDanhSach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableDanhSach.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.stt,
+            this.sp,
+            this.giamua,
+            this.ngaymua,
+            this.thoidiem,
+            this.trocap,
+            this.tietkiem,
+            this.id});
+            this.tableDanhSach.ContextMenuStrip = this.menuTable;
+            this.tableDanhSach.Location = new System.Drawing.Point(12, 12);
+            this.tableDanhSach.Name = "tableDanhSach";
+            this.tableDanhSach.RowTemplate.Height = 28;
+            this.tableDanhSach.Size = new System.Drawing.Size(1001, 586);
+            this.tableDanhSach.TabIndex = 0;
+            this.tableDanhSach.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tableDanhSach_CellClick);
+            // 
+            // stt
+            // 
+            this.stt.HeaderText = "STT";
+            this.stt.Name = "stt";
+            this.stt.ReadOnly = true;
+            this.stt.Width = 65;
+            // 
+            // sp
+            // 
+            this.sp.DataPropertyName = "sanpham";
+            this.sp.HeaderText = "Tên sản phẩm";
+            this.sp.Name = "sp";
+            this.sp.Width = 250;
+            // 
+            // giamua
+            // 
+            this.giamua.DataPropertyName = "giamua";
+            this.giamua.HeaderText = "Giá mua";
+            this.giamua.Name = "giamua";
+            this.giamua.Width = 130;
+            // 
+            // ngaymua
+            // 
+            this.ngaymua.DataPropertyName = "date";
+            this.ngaymua.HeaderText = "Ngày mua";
+            this.ngaymua.Name = "ngaymua";
+            this.ngaymua.Width = 130;
+            // 
+            // thoidiem
+            // 
+            this.thoidiem.DataPropertyName = "thoidiem";
+            this.thoidiem.HeaderText = "Thời điểm";
+            this.thoidiem.Name = "thoidiem";
+            this.thoidiem.Width = 123;
+            // 
+            // trocap
+            // 
+            this.trocap.DataPropertyName = "trocap";
+            this.trocap.HeaderText = "Trợ cấp";
+            this.trocap.Name = "trocap";
+            this.trocap.Width = 130;
+            // 
+            // tietkiem
+            // 
+            this.tietkiem.DataPropertyName = "tietkiem";
+            this.tietkiem.HeaderText = "Tiết kiệm";
+            this.tietkiem.Name = "tietkiem";
+            this.tietkiem.Width = 130;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // menuTable
+            // 
+            this.menuTable.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuTable.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateToolStripMenuItem,
+            this.xóaToolStripMenuItem});
+            this.menuTable.Name = "menuTable";
+            this.menuTable.Size = new System.Drawing.Size(143, 64);
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(142, 30);
+            this.updateToolStripMenuItem.Text = "Update";
+            this.updateToolStripMenuItem.Click += new System.EventHandler(this.updateToolStripMenuItem_Click);
+            // 
+            // xóaToolStripMenuItem
+            // 
+            this.xóaToolStripMenuItem.Name = "xóaToolStripMenuItem";
+            this.xóaToolStripMenuItem.Size = new System.Drawing.Size(142, 30);
+            this.xóaToolStripMenuItem.Text = "Xóa";
+            this.xóaToolStripMenuItem.Click += new System.EventHandler(this.xóaToolStripMenuItem_Click);
+            // 
+            // tong
+            // 
+            this.tong.AutoSize = true;
+            this.tong.Location = new System.Drawing.Point(838, 601);
+            this.tong.Name = "tong";
+            this.tong.Size = new System.Drawing.Size(93, 38);
+            this.tong.TabIndex = 8;
+            this.tong.Text = "label5";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 648);
+            this.Controls.Add(this.tong);
+            this.Controls.Add(this.tableDanhSach);
+            this.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form2";
+            this.Text = "Danh sách chi tiêu";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tableDanhSach)).EndInit();
+            this.menuTable.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tableDanhSach;
+        private System.Windows.Forms.Label tong;
+        private System.Windows.Forms.ContextMenuStrip menuTable;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xóaToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn giamua;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ngaymua;
+        private System.Windows.Forms.DataGridViewTextBoxColumn thoidiem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn trocap;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tietkiem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+    }
+}
---
+++ Chitieuhangthang/Chitieuhangthang/locTatCa.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chitieuhangthang
+{
+    public partial class locTatCa : Form
+    {
+        DataUtil obj = new DataUtil();
+        private string deleteID;
+        private int col;
+        private string _old;
+
+        public locTatCa()
+        {
+            InitializeComponent();
+        }
+
+        private void tabelLocAll_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int numrow;
+            numrow = e.RowIndex;
+            col = e.ColumnIndex;
+            _old = tabelLocAll.Rows[numrow].Cells[col].Value.ToString();
+            deleteID = tabelLocAll.Rows[numrow].Cells[2].Value.ToString();
+        }
+
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // lấy thông tin dữ liệu từ bảng
+            //List<ChitieuXuat> xuat = new List<ChitieuXuat>();
+            for (int i = 0; i < tabelLocAll.RowCount; i++)
+            {
+                // do sắp xếp trong Chitieu đặt khác nên cần truy xuất theo thứ tự của Chitieu
+                Chitieu chiTieu = new Chitieu();
+                chiTieu.id = tabelLocAll.Rows[i].Cells[2].Value.ToString();
+                chiTieu.sanpham = obj.chuanhoaSP(tabelLocAll.Rows[i].Cells[3].Value.ToString());
+                string gia = obj.chuanhoaSP(tabelLocAll.Rows[i].Cells[4].Value.ToString());
+                if(gia == "not")
+                {
+                    MessageBox.Show("Không thể update", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+                else
+                    chiTieu.giamua = gia;
+                chiTieu.date = tabelLocAll.Rows[i].Cells[1].Value.ToString();
+                chiTieu.thoidiem = tabelLocAll.Rows[i].Cells[5].Value.ToString();
+                if (tabelLocAll.Rows[i].Cells[6].Value.ToString() == "")
+                    chiTieu.trocap = "0";
+                else
+                    chiTieu.trocap = tabelLocAll.Rows[i].Cells[5].Value.ToString();
+                if (tabelLocAll.Rows[i].Cells[7].Value.ToString() == "")
+                    chiTieu.tietkiem = "0";
+                else
+                    chiTieu.tietkiem = tabelLocAll.Rows[i].Cells[6].Value.ToString();
+
+                if(chiTieu.id == deleteID)
+                {
+                    string _new = tabelLocAll.Rows[i].Cells[col].Value.ToString(); ;
+                    obj.updateLog("Lọc sản phẩm", DateTime.Today.ToShortDateString(), DateTime.Now.ToLongTimeString(), deleteID, _old, _new);
+                }
+
+                bool ktra = obj.Update(chiTieu);
+                if (!ktra)
+                {
+                    MessageBox.Show("Không thể cập nhật được", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            displayData();
+        }
+
+        private void xóaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult userAnswer = MessageBox.Show("Bạn có chắc chắn xóa", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            if (userAnswer == DialogResult.Yes)
+            {
+                obj.deleteLog("Lọc tất cả", DateTime.Today.ToShortDateString(), DateTime.Now.ToLongTimeString(), deleteID);
+                bool ktra = obj.Delete(deleteID);
+                if (!ktra)
+                {
+                    MessageBox.Show("Không thể xóa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                displayData();
+            }
+        }
+
+        private void locTatCa_Load(object sender, EventArgs e)
+        {
+            displayData();
+        }
+
+        private void displayData()
+        {
+            List<ChitieuXuat> xuat = new List<ChitieuXuat>();
+            xuat = obj.fillterItemDateAll();
+            tabelLocAll.DataSource = xuat;
+
+            //List<string> ds = new List<string>();
+            //ds = obj.ktraNgay(xuat);
+            //List<ChitieuXuat> ds1 = new List<ChitieuXuat>();
+            //ds1 = obj.ngayMax_Min(xuat);
+
+            //if (ds.Count == 1)
+            //{
+            //    maxKey.Text = ds1[0].date;
+            //    maxValue.Text = ds1[0].giamua + " - " + ds1[0].sanpham + " sản phẩm";
+            //    minKey.Text = ds1[0].date;
+            //    minValue.Text = ds1[0].giamua + " - " + ds1[0].sanpham + " sản phẩm";
+            //    tong.Text = ds1[0].giamua;
+            //}
+            //else
+            //{
+            //    int max = ds1.Max(danhsach1 => int.Parse(danhsach1.giamua));
+            //    int min = ds1.Min(danhsach1 => int.Parse(danhsach1.giamua));
+            //    for (int i = 0; i < ds1.Count; i++)
+            //    {
+            //        if (ds1[i].giamua == max.ToString())
+            //        {
+            //            maxKey.Text = ds1[i].date;
+            //            maxValue.Text = ds1[i].giamua + " - " + ds1[i].sanpham + " sản phẩm";
+            //        }
+            //        else if (ds1[i].giamua == min.ToString())
+            //        {
+            //            minKey.Text = ds1[i].date;
+            //            minValue.Text = ds1[i].giamua + " - " + ds1[i].sanpham + " sản phẩm";
+            //        }
+            //        else
+            //            continue;
+            //    }
+            //}
+            //tong.Text = xuat.Sum(thongtin => int.Parse(thongtin.giamua)).ToString();
+            if (xuat.Count == 0)
+                tong.Text = "0 sản phẩm";
+            else
+            {
+                tong.Text = xuat.Count.ToString() + " sản phẩm";
+                for (int i = 1; i <= xuat.Count; i++)
+                    tabelLocAll.Rows[i - 1].Cells[0].Value = i;
+            }
+        }
+    }
+}
